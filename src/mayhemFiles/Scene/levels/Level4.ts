e796086fc20d5e9e4333d99eef2d14e34c90ccc2
Lin Ni <lin.ni@stonybrook.edu>
2022-05-03 20:20:56 -0400

    addedtwomonsters

--- src/mayhemFiles/Scene/levels/Level4.ts
+++ src/mayhemFiles/Scene/levels/Level4.ts
@@ -4,7 +4,7 @@ export default class Level4 extends mainScene {
   loadScene() {
     this.load.audio("bgm", "mayhemAssets/music/bgm.mp3");
     this.load.tilemap("level", "mayhemAssets/tilemaps/level4.json");
-    this.load.spritesheet("enemy", "mayhemAssets/spritesheets/slime.json");
+    this.load.spritesheet("enemy", "mayhemAssets/spritesheets/gemstone.json");
     this.load.image("checkpoint", "mayhemAssets/sprites/checkpoint2.png");
 
     super.mainLoadScene();
